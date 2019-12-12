fe3eb64040f59fe3c9f8903acbbed6a34a8b870e
Geovanny Buitrago <geovanny91_1@hotmail.com>
2019-12-11 22:01:13 -0500

    Configuring firebaseMessaging var to get push notification: onMessage, on Launch, onResume.

--- lib/src/providers/push_nofitications_provider.dart
+++ lib/src/providers/push_nofitications_provider.dart
@@ -11,5 +11,23 @@ class PushNotificationProvider{
       print(token);
       //e79W7tiA_OI:APA91bGaDMobmSP4MdpQDRA-npMZ5b1nuDqWYmfLP8OtLNfTAgQ8wQztSgZkPqG9IrW25S4SCknPB3jr_xeO8SUvnkr9v-3FVly-xb62GKdsCPPVX1vmSn2JT_A6WN-OoSmGh-dVR4Tu
     });
+
+    _firebaseMessaging.configure(
+      onMessage: (data){
+        print('==== onMessage ====');
+        print(data);
+      },
+      onLaunch: (data){
+        print('==== onLaunch ====');
+        print(data);
+        
+      },
+      onResume: (data){
+        print('==== onResume ====');
+        print(data);   
+        //final notification = data['data']['food'];
+        //print(notification);
+      }
+    );
   }
 }
